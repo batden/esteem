3b4daa9862c2e9619b98a33aa1e702489f3b475f
Philippe Jean Guillaumie <similar@orange.fr>
2021-06-23 08:47:06 +0200

    Refactor bump_avf

--- esteem.sh
+++ esteem.sh
@@ -654,8 +654,8 @@ bump_avf() {
     cmake .. -DAVIF_CODEC_AOM=ON -DBUILD_SHARED_LIBS=OFF
     make
     sudo make install
-    rm -rf v$LAVF.tar.gz
-    rm -rf libavif-0.8.4
+    rm -rf $ESRC/v$LAVF.tar.gz
+    rm -rf $ESRC/libavif-0.8.4
     echo
   fi
 }
