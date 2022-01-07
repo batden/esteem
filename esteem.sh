39ab5dfe94b9875f13509323cd447a9e9cdc8d93
Philippe Jean Guillaumie <batden@sfr.fr>
2022-01-07 11:56:29 +0100

    Refactor sel_menu()

--- esteem.sh
+++ esteem.sh
@@ -146,7 +146,7 @@ menu_sel() {
 sel_menu() {
   if [ $INPUT -lt 1 ]; then
     echo
-    printf "1  $BDG%s $OFF%s\n\n" "INSTALL Enlightenment now"
+    printf "1  $LWG%s $OFF%s\n\n" "INSTALL Enlightenment now"
     printf "2  $BDG%s $OFF%s\n\n" "Update and REBUILD Enlightenment"
     printf "3  $BDP%s $OFF%s\n\n" "Update and rebuild Enlightenment in RELEASE mode"
     printf "4  $BDY%s $OFF%s\n\n" "Update and rebuild Enlightenment with WAYLAND support"
