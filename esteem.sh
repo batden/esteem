6ccaf791a9a454a07f0d5b63a772d44ee3623eb9
Philippe Jean Guillaumie <batden@sfr.fr>
2022-02-16 08:30:01 +0100

    Add comments

--- esteem.sh
+++ esteem.sh
@@ -113,7 +113,7 @@ PROG_MN="efl terminology enlightenment ephoto evisum rage express ecrire envento
 # FUNCTIONS
 # ---------
 
-# Audible feedback (error, sudo prompt...). Make sure the sound is turned on.
+# Audible feedback (error, sudo prompt...). Make sure the sound is turned on and loud enough.
 beep_attention() {
   paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
 }
