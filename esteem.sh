104228015ee3b178dc217d56fcee8aed224b7bbe
Philippe Jean Guillaumie <batden@sfr.fr>
2022-12-27 19:43:11 +0100

    Refactor do_bash_alias()

--- esteem.sh
+++ esteem.sh
@@ -596,7 +596,29 @@ do_tests() {
 }
 
 do_bsh_alias() {
-  if [ ! -f $HOME/.bash_aliases ]; then
+  if [ -f $HOME/.bash_aliases ]; then
+    mv -vb $HOME/.bash_aliases $HOME/.bash_aliases_bak
+    echo
+
+    touch $HOME/.bash_aliases
+
+    cat >$HOME/.bash_aliases <<EOF
+    # ----------------
+    # GLOBAL VARIABLES
+    # ----------------
+
+    # Compiler and linker flags added by ESTEEM.
+    export CC="ccache gcc"
+    export CXX="ccache g++"
+    export USE_CCACHE=1
+    export CCACHE_COMPRESS=1
+    export CPPFLAGS=-I/usr/local/include
+    export LDFLAGS=-L/usr/local/lib
+    export PKG_CONFIG_PATH=/usr/local/lib/x86_64-linux-gnu/pkgconfig:/usr/local/lib/pkgconfig
+EOF
+
+    source $HOME/.bash_aliases
+  else
     touch $HOME/.bash_aliases
 
     cat >$HOME/.bash_aliases <<EOF
