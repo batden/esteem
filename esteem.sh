f57925d9e49d75b8e3db3aecbdfb021509f83a02
Philippe Jean Guillaumie <batden@sfr.fr>
2023-03-23 17:00:19 +0100

    Refactor p_bkp()

--- esteem.sh
+++ esteem.sh
@@ -234,10 +234,6 @@ p_bkp() {
     rm /tmp/apt-avail
     dpkg --get-selections >$DOCDIR/pbackups/installed_pkgs.log
 
-    # Backup list of manually installed DEB packages.
-    echo $(comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz |
-      sed -n 's/^Package: //p' | sort -u)) >$DOCDIR/pbackups/manually_installed_pkgs.txt
-
     # Backup list of available repositories.
     grep -Erh ^deb /etc/apt/sources.list* >$DOCDIR/pbackups/available_repos.txt
 
