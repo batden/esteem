9bc2c72e33d625fd5e96628955ce8d4d49652ce4
Philippe Jean Guillaumie <batden@sfr.fr>
2023-04-11 08:31:45 +0200

    Remove chk_efte()

--- esteem.sh
+++ esteem.sh
@@ -350,7 +350,6 @@ rebuild_optim() {
   bin_deps
   e_tokens
   chk_ddcl
-  chk_efte
   chk_fcst
   elap_start
 
@@ -467,7 +466,6 @@ rebuild_wld() {
   bin_deps
   e_tokens
   chk_ddcl
-  chk_efte
   chk_fcst
   elap_start
 
@@ -726,19 +724,6 @@ chk_ddcl() {
     sudo ldconfig
     rm -rf $DLDIR/v$DDTL.tar.gz
     echo
-  fi
-}
-
-chk_efte() {
-  if [ ! -d $ESRC/e26/eflete ]; then
-    cd $ESRC/e26
-    printf "\n$BLD%s $OFF%s\n" "Adding Eflete application to the ecosystem..."
-    sleep 1
-    $CLONETE
-    cd $ESRC/e26/eflete
-    $AUTGN
-    make
-    $SMIL
   fi
 }
 
