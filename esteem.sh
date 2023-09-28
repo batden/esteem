9fe8a3e78fa5c2c59242e891ce9d4f3667e48b05
Philippe Jean Guillaumie <batden@sfr.fr>
2023-09-28 14:21:41 +0200

    Minor corrections

--- esteem.sh
+++ esteem.sh
@@ -691,8 +691,7 @@ chk_ddcl() {
   if [ $DISTRO == kinetic ] && [ -d $ESRC/ddcutil-1.4.1 ]; then
     printf "\n$BLD%s $OFF%s\n" "Updating ddcutil..."
     sleep 1
-    sudo apt install libjansson-dev
-    echo
+    sudo apt install libjansson-dev &>/dev/null
     cd $ESRC/ddcutil-1.4.1
     sudo make uninstall &>/dev/null
     cd .. && rm -rf $ESRC/ddcutil-1.4.1
