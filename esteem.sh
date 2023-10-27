45ccf00f733b018018343dfec62ff2dd30f72d6f
Philippe Jean Guillaumie <batden@sfr.fr>
2023-10-27 13:29:01 +0200

    Minor correction

--- esteem.sh
+++ esteem.sh
@@ -32,7 +32,7 @@
 # This work is licensed under a Creative Commons Attribution-ShareAlike 4.0
 # International License, in memory of Aaron Swartz.
 
-# Got a Github account? Please consider starring our repositories to show your support.
+# Got a GitHub account? Please consider starring our repositories to show your support.
 # Cheers!
 
 # ---------------
