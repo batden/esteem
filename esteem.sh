9efb4adc8340c955155b8d76713286c04cdbc0f7
Philippe Jean Guillaumie <batden@sfr.fr>
2023-12-15 09:51:53 +0100

    Add comments

--- esteem.sh
+++ esteem.sh
@@ -783,7 +783,10 @@ wld_go() {
   exit 0
 }
 
-# Lo and behold!
+# Lo and behold (bhd)!
+#
+# Displays the selection menu...
+#
 lo() {
   trap '{ printf "\n$BDR%s $OFF%s\n\n" "KEYBOARD INTERRUPT."; exit 130; }' INT
 
@@ -797,6 +800,7 @@ lo() {
   fi
 }
 
+# and gets the user's choice.
 bhd() {
   if [ $INPUT == 1 ]; then
     do_tests
