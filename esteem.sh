8311c0cc5e76ba56d8e64f85858e97b9487fb437
Philippe Jean Guillaumie <batden@sfr.fr>
2023-12-20 14:46:32 +0100

    Add cnt_dir()

--- esteem.sh
+++ esteem.sh
@@ -193,7 +193,7 @@ bin_deps() {
 cnt_dir() {
   COUNT=$(find . -mindepth 1 -maxdepth 1 -type d | wc -l)
   case $COUNT in
-  14)
+  13)
     printf "$BLDG%s $OFF%s\n\n" "All programs have been downloaded successfully."
     beep_dl_complete
     sleep 2
@@ -216,8 +216,8 @@ cnt_dir() {
       # you previously used.
     fi
 
-    if [ $COUNT -lt 14 ]; then
-      printf "\n$BLDY%s %s\n" "WARNING: ONLY $COUNT OF 14 PROGRAMS HAVE BEEN DOWNLOADED!"
+    if [ $COUNT -lt 13 ]; then
+      printf "\n$BLDY%s %s\n" "WARNING: ONLY $COUNT OF 13 PROGRAMS HAVE BEEN DOWNLOADED!"
       printf "\n$BLDY%s $OFF%s\n\n" "WAIT 12 SECONDS OR HIT CTRL+C TO EXIT NOW."
       beep_attention
       sleep 12
