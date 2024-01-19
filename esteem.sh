01e1ef3b2f7abad68813867dd6b8567e3af3c499
Philippe Jean Guillaumie <batden@sfr.fr>
2024-01-19 08:54:52 +0100

    Formatting

--- esteem.sh
+++ esteem.sh
@@ -65,10 +65,10 @@ DISTRO=$(lsb_release -sc)
 # Build dependencies, recommended and script-related packages.
 DEPS="acpid arc-theme aspell build-essential ccache check cmake cowsay ddcutil doxygen \
 fonts-noto freeglut3-dev graphviz gstreamer1.0-libav gstreamer1.0-plugins-bad \
-gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly hwdata i2c-tools imagemagick libaom-dev \
-libasound2-dev libavahi-client-dev libavif-dev libblkid-dev libbluetooth-dev \
-libegl1-mesa-dev libexif-dev libfontconfig-dev libdrm-dev libfreetype-dev \
-libfribidi-dev libgbm-dev libgeoclue-2-dev libgif-dev libgraphviz-dev \
+gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly hwdata i2c-tools imagemagick \
+libaom-dev libasound2-dev libavahi-client-dev libavif-dev libblkid-dev \
+libbluetooth-dev libegl1-mesa-dev libexif-dev libfontconfig-dev libdrm-dev \
+libfreetype-dev libfribidi-dev libgbm-dev libgeoclue-2-dev libgif-dev libgraphviz-dev \
 libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libharfbuzz-dev \
 libheif-dev libi2c-dev libibus-1.0-dev libinput-dev libinput-tools \
 libjpeg-dev libjson-c-dev libjxl-dev libkmod-dev liblua5.2-dev liblz4-dev \
